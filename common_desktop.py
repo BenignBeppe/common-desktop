bdac53a500161d313d405bb9ee1552771c9a8d83
Sebastian Berlin <berlin.sebastian@gmail.com>
2017-05-25 11:36:36 +0200

    Don't show old images and marking images as favorites
    Added command to mark images as favorites. These will be kept and
    considered again when picking a new image. Added value for when an
    image was last shown, to not pick non favourite images that has
    already been shown. The files for non favourite images are also
    removed when a new image is shown.
    
    Added argument to --fetch for specifying the number of ids that should
    be fetched. Changed how the check for if an id is already in the
    database is made. Increased the number of ids to get in the API query
    to 500.
    
    Changed stdout logging to show info level, rather than debug. Debug is
    still used for the log files.

--- common_desktop.py
+++ common_desktop.py
@@ -19,35 +19,47 @@ URL = "https://commons.wikimedia.org/w/api.php"
 GNOME_SET_BACKGROUND_COMMAND = "gsettings set org.gnome.desktop.background picture-uri file://{image_path}"
 MATE_SET_BACKGROUND_COMMAND = "gsettings set org.mate.background picture-filename {image_path}"
 COMMONS_PAGE_BY_ID = "https://commons.wikimedia.org/w/?curid={page_id}"
+DEFAULT_FETCH_AMOUNT=100
+# Statuses for an image.
+FAVORITE = 1
 
 def setup_loggin(print_log):
     ensure_path_exists(LOGS_PATH)
     log_path = "{}/{}.log".format(LOGS_PATH, datetime.datetime.now())
     logging.basicConfig(filename=log_path, level=logging.DEBUG)
     if print_log:
-        logging.getLogger().addHandler(logging.StreamHandler())
+        stream_handler = logging.StreamHandler()
+        stream_handler.setLevel(logging.INFO)
+        logging.getLogger().addHandler(stream_handler)
 
 def ensure_path_exists(path):
     if not os.path.exists(path):
         os.makedirs(path)
 
-def populate_table(connection, category):
+def populate_table(connection, category, number_of_pages_to_fetch):
     ensure_images_table_exists(connection)
+    number_of_added_pages = 0
     for page_id in get_page_ids(category):
-        try:
+        page = connection.execute(
+            "SELECT * FROM images WHERE id={}".format(page_id)
+        ).fetchone()
+        if page is not None:
+            logging.info("Id {} already exists, skipping.".format(page_id))
+        else:
             connection.execute(
-                "INSERT INTO images VALUES({id}, 0)".format(id=page_id)
+                "INSERT INTO images VALUES({id}, 0, 0, 0)".format(id=page_id)
             )
             connection.commit()
-            logging.info("Added page id to database: %s", page_id)
-        except sqlite3.IntegrityError:
-            # Skip if the id is already in the table.
-            logging.debug("Didn't add page id %s to database. Probably since it was already present.", page_id)
-            continue
-    connection.close()
+            logging.info("Added page id to database: {}".format(page_id))
+            number_of_added_pages += 1
+        if number_of_added_pages == number_of_pages_to_fetch:
+            break
+    logging.info(
+        "Added {} new pages to database.".format(number_of_added_pages)
+    )
 
 def ensure_images_table_exists(connection):
-    connection.execute("CREATE TABLE IF NOT EXISTS images (id int UNIQUE, current boolean DEFAULT 0)")
+    connection.execute("CREATE TABLE IF NOT EXISTS images (id int UNIQUE, current boolean, status int, last_shown int)")
     connection.commit()
 
 def get_page_ids(category):
@@ -57,6 +69,7 @@ def get_page_ids(category):
         "list": "categorymembers",
         "cmtitle": category,
         "cmtype": "file|subcat",
+        "cmlimit": "500",
         "continue": ""
     }
     while True:
@@ -78,35 +91,49 @@ def get_page_ids(category):
         parameters["cmcontinue"] = response["continue"]["cmcontinue"]
 
 def send_request(parameters):
-    logging.debug("PARAMETERS: %s", parameters)
+    logging.debug("PARAMETERS: {}".format(parameters))
     data = urllib.parse.urlencode(parameters)
     request = urllib.request.Request(URL, data.encode())
-    logging.debug("REQUEST: %s?%s", request.get_full_url(), data)
+    logging.debug("REQUEST: {}?{}".format(request.get_full_url(), data))
     response_string = urllib.request.urlopen(request).read().decode()
-    logging.debug("RESPONSE: %s", response_string)
+    logging.debug("RESPONSE: {}".format(response_string))
     response = json.loads(response_string)
     return response
 
 def change_image(connection):
-    old_page_id = get_current_id(connection)
-    if old_page_id:
+    old_id = get_current_id(connection)
+    if old_id is not None:
+        status = connection.execute(
+            "SELECT status FROM images WHERE id={}".format(old_id)
+        ).fetchone()[0]
+        if status != FAVORITE:
+            old_path = get_path_for_page_id(old_id)
+            logging.info("Removing image: {}.".format(old_path))
+            os.remove(old_path)
         connection.execute(
-            "REPLACE INTO images VALUES({id}, 0)".format(id=old_page_id)
+            "UPDATE images SET current=0, last_shown=strftime('%s','now') WHERE id={}".format(old_id)
         )
     page_id = pick_page(connection)
-    if not image_exists(page_id):
-        image_url = get_image_url(page_id)
-        download_image(image_url, page_id)
-    image_path = get_path_for_page_id(page_id)
-    set_desktop_image(image_path)
-    connection.execute(
-        "REPLACE INTO images VALUES({id}, 1)".format(id=page_id)
-    )
+    if page_id is None:
+        logging.info("No new image to switch to, keeping current one.");
+    else:
+        if not image_exists(page_id):
+            image_url = get_image_url(page_id)
+            download_image(image_url, page_id)
+        image_path = get_path_for_page_id(page_id)
+        set_desktop_image(image_path)
+        connection.execute(
+            "UPDATE images SET current=1 WHERE id={}".format(page_id)
+        )
     connection.commit()
 
 def pick_page(connection):
-    page_ids = connection.execute("SELECT id FROM images").fetchall()
-    return random.choice(page_ids)[0]
+    # Pick from favorites and images that hasn't been shown yet.
+    page_ids = connection.execute(
+        "SELECT id FROM images WHERE status=1 or last_shown=0"
+    ).fetchall()
+    if page_ids:
+        return random.choice(page_ids)[0]
 
 def image_exists(page_id):
     return get_path_for_page_id(page_id)
@@ -133,14 +160,14 @@ def get_image_url(page_id):
 def download_image(image_url, page_id):
     file_ending = os.path.splitext(image_url)[1]
     path = "{}/{}{}".format(IMAGES_PATH, page_id, file_ending)
-    logging.info("Downloading %s to %s.", image_url, path)
+    logging.info("Downloading {} to {}.".format(image_url, path))
     urllib.request.urlretrieve(image_url, path)
 
 def set_desktop_image(image_path):
     command = get_set_background_command().format(
         image_path=os.path.abspath(image_path)
     )
-    logging.info("Running: %s", command)
+    logging.info("Running: {}".format(command))
     subprocess.call(command.split())
 
 def get_set_background_command():
@@ -166,43 +193,73 @@ def get_current_id(connection):
     current_id = connection.execute(
         "SELECT id FROM images WHERE current=1"
     ).fetchone()
-    if current_id != None:
+    if current_id is not None:
         return current_id[0]
+
+def set_current_status(connection, new_status):
+    current_id = get_current_id(connection)
+    logging.info(
+        "Setting status of current image (id={}) to: {}."
+        .format(current_id, new_status)
+    )
+    connection.execute(
+        "UPDATE images SET status={} WHERE id={}".format(
+            new_status, current_id
+        )
+    )
+    connection.commit()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--fetch-ids",
         "-f",
-        help="Update the page ids in the database by getting the page ids from CATEGORY and its subcategories.",
-        metavar="CATEGORY"
+        nargs="+",
+        metavar=("CATEGORY", "AMOUNT"),
+        help="Update the page ids in the database by getting the page ids from CATEGORY and its subcategories. AMOUNT is the maximum number of ids that will be fetched, default {}.".format(DEFAULT_FETCH_AMOUNT)
     )
     parser.add_argument(
         "--print-log",
         "-l",
-        help="Write log messages to stderr as well as log file.",
-        action="store_true"
+        action="store_true",
+        help="Write log messages to stderr as well as log file."
     )
     parser.add_argument(
         "--new-image",
         "-n",
-        help="Change to a new desktop image.",
-        action="store_true"
+        action="store_true",
+        help="Change to a new desktop image."
     )
     parser.add_argument(
         "--information",
         "-i",
-        help="Open the page for the current image on Wikimedia Commons in the default web browser.",
-        action="store_true"
+        action="store_true",
+        help="Open the page for the current image on Wikimedia Commons in the default web browser."
+    )
+    parser.add_argument(
+        "--favorite",
+        "-a",
+        action="store_true",
+        help="Marks the current image as a favorite. Favorites won't be deleted when swithing to a new image."
     )
     args = parser.parse_args()
     setup_loggin(args.print_log)
     ensure_path_exists(IMAGES_PATH)
     connection = sqlite3.connect(DB_PATH)
     if args.fetch_ids:
-        populate_table(connection, "Category:{}".format(args.fetch_ids))
+        if len(args.fetch_ids) > 1:
+            amount = int(args.fetch_ids[1])
+        else:
+            amount = DEFAULT_FETCH_AMOUNT
+        populate_table(
+            connection,
+            "Category:{}".format(args.fetch_ids[0]),
+            amount
+        )
     if args.new_image:
         change_image(connection)
     if args.information:
         show_image_page(connection)
+    if args.favorite:
+        set_current_status(connection, FAVORITE)
     connection.close()
